c49a8070bcf0564b64e77b038e1363f61844f13f
ma-jo-ne <2215xyago>
2014-09-24 12:36:13 +0200

    Player Tabelle kann erstellt werden und Player können hinzugefügt werden

---
+++ js/Team.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    Parse.initialize("9nPPbQxM1lKkfOOSiJWDiVhP1Ze6leFgeKNxWvTz", "3212hWENS0Iv0CHmFgZh4gfgP9s3vJnLeRsHVbPN");
+
+    $('#button-add-player').on('click', function () {
+        addNewPlayer("add-player");
+    });
+    $('#button-add-next-player').on('click', function () {
+        addNewPlayer("add-next-player");
+    });
+});
+
+function addNewPlayer(string) {
+    var playerName = $('#input-player-name').val();
+    if (playerName != "") {
+        if (string == "add-player") {
+            savePlayer(playerName);
+            $('#modal-add-player').foundation('reveal', 'close');
+            $('#input-player-name').val("");
+            $('#input-player-name').attr("placeholder", "Vorname Nachname");
+        } else {
+            savePlayer(playerName);
+            $('#input-player-name').val("");
+            $('#input-player-name').attr("placeholder", "Vorname Nachname");
+        }
+    }
+}
+
+function savePlayer(playerName) {
+    var teamName = Parse.User.current()['attributes']['teamname'] + "_players";
+    var Tabelle = Parse.Object.extend(teamName);
+    var players = new Tabelle();
+    players.set("playerName", playerName);
+    players.save(null, {
+        success: function (players) {
+            // Execute any logic that should take place after the object is saved.
+            //@todo: super fancy teamerstellungsmeldung
+            alert('New object created with objectId: ' + players);
+        },
+        error: function (players, error) {
+            // Execute any logic that should take place if the save fails.
+            // error is a Parse.Error with an error code and message.
+            alert('Failed to create new object, with error code: ' + error.message);
+        }
+    });
+
+}
