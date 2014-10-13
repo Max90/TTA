6a6aaff4dff125874247db3725ca7621d9538e26
Max90 <maximilian.feigl@stud.uni-regensburg.de>
2014-10-13 10:00:42 +0200

    Funktionalität für context menü eingebaut

--- js/training.js
+++ js/training.js
@@ -7,6 +7,25 @@ $(document).ready(function () {
     });
 
     showTrainingList();
+
+    $.contextMenu({
+        selector: '.context-menu-one',
+        trigger: 'left',
+
+        items: {
+            "addToTraining": {name: "Zu Training hinzufügen/entfernen", icon: "edit"},
+            "addNutmeg": {name: "Beinschuss hinzufügen", icon: "cut"}
+//            "copy": {name: "Copy", icon: "copy"},
+//            "paste": {name: "Paste", icon: "paste"},
+//            "delete": {name: "Delete", icon: "delete"},
+//            "sep1": "---------",
+//            "quit": {name: "Quit", icon: "quit"}
+        }
+    });
+
+    $('.context-menu-one').on('click', function (e) {
+        console.log('clicked', this);
+    });
 });
 
 function addNewTraining() {
@@ -17,7 +36,8 @@ function addNewTraining() {
     saveTraining(dateTraining, timeTraining);
 
 }
-var inTraining = false;
+
+
 function showTrainingList() {
     var teamName = Parse.User.current()['attributes']['teamname'] + "_trDates";
     var trDates = Parse.Object.extend(teamName);
