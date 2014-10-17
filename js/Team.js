b3e4417bee5bf3634cdb01924ad8965bcd17d975
ma-jo-ne <2215xyago>
2014-10-17 13:10:17 +0200

    ---

--- js/Team.js
+++ js/Team.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
 
     Parse.initialize("9nPPbQxM1lKkfOOSiJWDiVhP1Ze6leFgeKNxWvTz", "3212hWENS0Iv0CHmFgZh4gfgP9s3vJnLeRsHVbPN");
+    $('#button-logout').on('click', function () {
+        Parse.User.logOut();
+
+        var currentUser = Parse.User.current();
+        checkIfUserLoggedIn();
+    });
 
     $('#button-add-player').on('click', function () {
         addNewPlayer("add-player");
@@ -11,6 +17,16 @@ $(document).ready(function () {
 
     showPlayers();
 });
+
+function checkIfUserLoggedIn() {
+    var currentUser = Parse.User.current();
+    if (currentUser) {
+        $('#button-logout').show();
+    } else {
+        $('#button-logout').hide();
+        $('.login-form').show();
+    }
+}
 
 function addNewPlayer(string) {
     var playerName = $('#input-player-name').val();
--- js/training.js
+++ js/training.js
@@ -2,6 +2,13 @@ $(document).ready(function () {
 
     Parse.initialize("9nPPbQxM1lKkfOOSiJWDiVhP1Ze6leFgeKNxWvTz", "3212hWENS0Iv0CHmFgZh4gfgP9s3vJnLeRsHVbPN");
 
+    $('#button-logout').on('click', function () {
+        Parse.User.logOut();
+
+        var currentUser = Parse.User.current();
+        checkIfUserLoggedIn();
+    });
+
     $('#button-add-training').on('click', function () {
         addNewTraining();
     });
@@ -15,6 +22,17 @@ $(document).ready(function () {
     showTrainingList();
 
 });
+
+
+function checkIfUserLoggedIn() {
+    var currentUser = Parse.User.current();
+    if (currentUser) {
+        $('#button-logout').show();
+    } else {
+        $('#button-logout').hide();
+        $('.login-form').show();
+    }
+}
 
 function addNewTraining() {
     var dateTraining = $('#input-date-training').val();
