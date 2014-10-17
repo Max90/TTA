81165ec39c7965d1de9e3a9da02e5e4f5b72fe34
Max90 <maximilian.feigl@stud.uni-regensburg.de>
2014-10-17 17:26:46 +0200

    upload geht auch für small

--- js/Team.js
+++ js/Team.js
@@ -104,14 +104,14 @@ function uploadFile (playerName) {
     var file;
 
     // Set an event listener on the Choose File field.
-    $('#fileselect').bind("change", function (e) {
+    $('.fileselect').bind("change", function (e) {
         var files = e.target.files || e.dataTransfer.files;
         // Our file var now holds the selected file
         file = files[0];
     });
 
     // This function is called when the user clicks on Upload to Parse. It will create the REST API request to upload this image to Parse.
-    $('#uploadbutton').click(function () {
+    $('.uploadbutton').click(function () {
         var serverUrl = 'https://api.parse.com/1/files/' + file.name;
 
         $.ajax({
