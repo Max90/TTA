42bb11182a2cc32bcc489373b935b51452ada6e3
Max90 <maximilian.feigl@stud.uni-regensburg.de>
2014-10-22 18:29:14 +0200

    bei beiner modal muss noch rechtzeitig undefineder spieler gelöscht werden

--- manager/js/nutmeg.js
+++ manager/js/nutmeg.js
@@ -23,14 +23,10 @@ function showTrainingListNutmeg() {
 
     query.find({
         success: function (results) {
+            showPlayersForNutmegModal(results[0].get('dateTraining'));
             // Do something with the returned Parse.Object values
             for (var i = 0; i < results.length; i++) {
                 var object = results[i];
-                /* var d = new Date(object.get('dateTraining'));
-                 var curr_date = d.getDate();
-                 var curr_month = d.getMonth() + 1; //Months are zero based
-                 var curr_year = d.getFullYear();
-                 var formatedTrDate = curr_date + "." + curr_month + "." + curr_year;*/
 
                 $("#training-table").append($("<tr href='#' data-reveal-id='modal-add-nutmeg-to-player'>").append($('<td>'
                     + object.get('dateTraining') + '</td>' + '<td>' + object.get('timeTraining') + '</td>')).on("click", function () {
@@ -41,8 +37,6 @@ function showTrainingListNutmeg() {
                     var curr_month = d.getMonth() + 1; //Months are zero based
                     var curr_year = d.getFullYear();
                     $('#modal-add-nutmeg-to-player').find('#header-date').text("Datum: " + curr_date + "." + curr_month + "." + curr_year);
-
-                    showPlayersForNutmegModal($(this).closest('tr').children('td:first').text());
                 }));
 
             }
@@ -54,18 +48,25 @@ function showTrainingListNutmeg() {
 }
 
 function showPlayersForNutmegModal(dateTraining) {
-
-
     //umgehen des neuen spalten problems hier
     var teamName = Parse.User.current()['attributes']['teamname'] + "_players";
     var Tabelle = Parse.Object.extend(teamName);
 
     var training = new Tabelle();
-    training.set("nm", 0);
-    training.save();
+
+    var date = dateTraining.replace(/-/g, "_");
+    var columname = "nm_" + date;
+    training.set(columname, 0);
+    training.save(null, {
+        success: function (results) {
+            deleteUndefinedPlayer();
+        },
+        error: function (error) {
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
 //ende umgehen hier
 
-    var teamName = Parse.User.current()['attributes']['teamname'] + "_players";
     var team = Parse.Object.extend(teamName);
     var query = new Parse.Query(team);
     query.ascending("playerName");
@@ -84,14 +85,25 @@ function showPlayersForNutmegModal(dateTraining) {
 
 }
 
+function deleteUndefinedPlayer(){
+    var teamName = Parse.User.current()['attributes']['teamname'] + "_players";
+    var team = Parse.Object.extend(teamName);
+    var query = new Parse.Query(team);
+    query.equalTo("playerName", undefined);
+
+    query.first({
+        success: function (myObj) {
+            myObj.destroy({});
+        },
+        error: function (error) {
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
+}
+
 function listPlayerInNutmegModal(playerName, dateTraining) {
-
-
-
-
-
-
-    /* var query = new Parse.Query(team);
+    var teamName = Parse.User.current()['attributes']['teamname'] + "_players";
+    var query = new Parse.Query(teamName);
      query.equalTo("playerName", playerName);
     query.ascending("playerName");
     query.first({
@@ -104,62 +116,14 @@ function listPlayerInNutmegModal(playerName, dateTraining) {
             }
             $("#player-modal-table").append($('<tr class="player-context-menu">').append($('<td><img src="' + imgSrc + '"></td>' + '<td>'
                 + playerName + '</td>')).on("click", function () {
-                addNutmegToPlayer($(this), $(this).closest('tr').children('td:last').text(), dateTraining);
+                updateNutmegTrCount(playerName, dateTraining, 1)
             }));
 
         },
         error: function (error) {
             alert("Error: " + error.code + " " + error.message);
         }
-     });*/
-}
-
-
-function addNutmegToPlayer(object, playerName, dateTraining) {
-    var teamNameNutmeg = Parse.User.current()['attributes']['teamname'] + "_players";
-
-    var Tabelle = Parse.Object.extend(teamNameNutmeg);
-    Tabelle.put("nm-" + dateTraining, 0);
-    nutmegTable.save(null, {
-        success: function (training) {
-
-        },
-        error: function (training, error) {
-            // Execute any logic that should take place if the save fails.
-            // error is a Parse.Error with an error code and message.
-            console.log('Failed to create new object, with error code: ' + error.message);
-        }
-    });
-
-
-    var query = new Parse.Query(Tabelle);
-    query.equalTo("playerName", playerName);
-    query.first({
-        success: function (players) {
-                updateNutmegTrCount(playerName, dateTraining, 1);
-
-        },
-        error: function (players, error) {
-            console.log('Failed to create new object, with error code: ' + error.message);
-        }
-    });
-}
-
-function createNutmegTable (playerName, dateTraining) {
-    var teamNameTraining = Parse.User.current()['attributes']['teamname'] + "_players";
-
-    var Tabelle = Parse.Object.extend(teamNameTraining);
-    var query = new Parse.Query(Tabelle);
-    query.equalTo("playerName", playerName);
-    query.first({
-        success: function (player) {
-            player.set("nm-" + dateTraining, 1);
-            player.save();
-        },
-        error: function (players, error) {
-            console.log('Failed to create new object, with error code: ' + error.message);
-        }
-    });
+     });
 }
 
 
@@ -168,11 +132,19 @@ function updateNutmegTrCount(playerName, dateTraining, count) {
     var teamName = Parse.User.current()['attributes']['teamname'] + "_players";
     var team = Parse.Object.extend(teamName);
     var query = new Parse.Query(team);
+    var date = dateTraining.replace(/-/g, "_");
+    console.log(date);
     query.equalTo("playerName", playerName);
     query.first({
         success: function (player) {
-            var trCount = player.get("nutmegCount") + count;
-            player.set("tr-" + dateTraining, trCount);
+            var temp;
+            if(player.get('nutmegCoung') == undefined){
+                temp = 0
+            } else {
+                temp = player.get('nutmegCount');
+            }
+            var trCount = temp + count;
+            player.set("nm_" + date, trCount);
             player.save();
         },
         error: function (players, error) {
